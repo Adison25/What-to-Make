1760036eef6694dbca167cd249fc39b7a53af006
Adison25 <emerickadison@gmail.com>
2021-08-24 07:59:17 -0700

    loading screen done

--- What to Make/controllers/pageViews/LoadingViewController.swift
+++ What to Make/controllers/pageViews/LoadingViewController.swift
@@ -6,36 +6,41 @@
 //
 
 import UIKit
+import SkeletonView
 
 class LoadingViewController: UIViewController {
-
+    
+    lazy var myView : UIView = {
+        let view = UIView()
+        view.backgroundColor = .systemGray6
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        //make it look like its loading
+        view.addSubview(myView)
+        NSLayoutConstraint.activate([
+            myView.topAnchor.constraint(equalTo: view.topAnchor),
+            myView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            myView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            myView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+        myView.isSkeletonable = true
+        myView.showAnimatedSkeleton(usingColor: .systemGray6, animation: nil, transition: .crossDissolve(0.25))
+        
         fetchData { data in
             defaults.set(true, forKey: "LoggedIn")
             DispatchQueue.main.async {
                 let vc = (UIStoryboard(name: "Main",bundle: nil).instantiateViewController(withIdentifier: Constants.Storyboard.tabBarVC) as! TabBarController)
                 vc.modalTransitionStyle = .crossDissolve
                 vc.modalPresentationStyle = .fullScreen
+                print("in")
                 self.present(vc, animated: true)
             }
-
+            
         }
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
