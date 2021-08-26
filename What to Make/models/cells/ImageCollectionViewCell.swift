1f17d2dd8e2822f7d85ad34afc9d98b8b882086e
Adison25 <emerickadison@gmail.com>
2021-08-26 08:42:49 -0700

    added a  waterfall layout to the bookmarked view controller. Now i gotta decide if i want both layouts or one as both of  them

---
+++ What to Make/models/cells/ImageCollectionViewCell.swift
@@ -0,0 +1,41 @@
+//
+//  ImageCollectionViewCell.swift
+//  What to Make
+//
+//  Created by Adison Emerick on 8/26/21.
+//
+
+import UIKit
+
+class ImageCollectionViewCell: UICollectionViewCell {
+    static let identifier = "ImageCollectionViewCell"
+    
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.addSubview(imageView)
+        contentView.clipsToBounds = true
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        imageView.frame = contentView.bounds
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+    }
+    
+    func configure(image: UIImage?) {
+        imageView.image = image
+    }
+}
