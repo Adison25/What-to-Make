5ea4dfe48a2e0ef50407a76229fc9780f62e9b84
Adison25 <emerickadison@gmail.com>
2021-09-03 09:33:10 -0700

    now tableview reloads data when purchased

--- What to Make/controllers/pageViews/SettingsViewController.swift
+++ What to Make/controllers/pageViews/SettingsViewController.swift
@@ -166,6 +166,7 @@ class SettingsViewController: UIViewController, UITableViewDelegate, UITableView
                 print("purchased")
                 SKPaymentQueue.default().finishTransaction($0)
                 defaults.set(true, forKey: "adsRemoved")
+                tableView.reloadData()
             case .failed:
                 print("failed")
                 SKPaymentQueue.default().finishTransaction($0)
@@ -173,6 +174,7 @@ class SettingsViewController: UIViewController, UITableViewDelegate, UITableView
                 print("restored")
                 SKPaymentQueue.default().finishTransaction($0)
                 defaults.set(true, forKey: "adsRemoved")
+                tableView.reloadData()
             case .deferred:
                 print("deferred")
             @unknown default:
